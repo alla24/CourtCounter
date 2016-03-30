ebfc03a9cae573ceca89346c927c790ff7155e42
Alla <alla.sadovets@gmail.com>
2016-03-30 22:08:13 +0300

    Javadoc added

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -15,46 +15,82 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**Add three Points and update the score for Team A
+     *
+     * @param view
+     */
     public void threePointsTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
+    /**Add two Points and update the score for Team A
+     *
+     * @param view
+     */
     public void twoPointsTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
+    /**Add one Point and update the score of Team A
+     *
+     * @param view
+     */
     public void freeThrowTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
+    /**Display new score for Team A
+     *
+     * @param score is new score for Team A
+     */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**Add three Points and update the score of Team B
+     *
+     * @param view
+     */
     public void threePointsTeamB(View view) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
+    /**Add two Points and update the score of Team B
+     *
+     * @param view
+     */
     public void twoPointsTeamB(View view) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
+    /**Add one Point and update the score of Team B
+     *
+     * @param view
+     */
     public void freeThrowTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
+    /**Display new score for Team B
+     *
+     * @param score is new score for Team B
+     */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**Reset score for Team A and Team B
+     *
+     * @param view
+     */
     public void reset(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
